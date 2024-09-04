a43a4e8b5da69bb00ff6456855ee830541e7011e
nadim <nadim@enginzyme.com>
2024-09-04 10:44:41 +0200

    feat: add mockup_data.py for testing purposes

---
+++ mockup_data.py
@@ -0,0 +1,21 @@
+from models import Sample, PlateSamples
+
+def generate_mockup_data() -> PlateSamples:
+    samples = []
+    starting_compounds = ["Compound A", "Compound B", "Compound C"]
+    processes = ["Process 1", "Process 2", "Process 3"]
+    enzymes = ["Enzyme X", "Enzyme Y", "Enzyme Z"]
+
+    for i in range(1, 49):  # 48 samples
+        sample_name = f"Sample {i}"
+        position = f"{chr(65 + (i - 1) // 8)}{(i - 1) % 8 + 1}"  # A1, A2, ..., F8
+        sample = Sample(
+            sample_name=sample_name,
+            position=position,
+            starting_compound=starting_compounds[i % len(starting_compounds)],
+            process=processes[i % len(processes)],
+            enzyme=enzymes[i % len(enzymes)]
+        )
+        samples.append(sample)
+
+    return PlateSamples(samples=samples)
